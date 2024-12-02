f834b667e699364809f6bba45e3cdb2850ef9069
shubhanshu <shubhanshukrssm10@gmail.com>
2024-12-03 00:05:49 +0530

    first commit

---
+++ Js/main.js
@@ -0,0 +1,13 @@
+const toggle =()=>{
+    if (document.querySelector("ul").style.left=="100%") {
+        document.querySelector("ul").style.left="0%"
+        
+    } else {
+        document.querySelector("ul").style.left="100%"
+    }
+}
+
+
+const sub =()=>{
+    alert("Your Email Is Submitted");
+}
